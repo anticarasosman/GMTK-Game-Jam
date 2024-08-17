45cbc3eb819ee659c9b10a6fcaa90bc3c2899c64
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-08-17 13:54:57 -0400

    Feat: se puede poner items

---
+++ movimiento_arbol/scripts/target_location_3d.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@export var Item = "Decoration"
+var completed = false
+
+func _ready() -> void:
+	self.body_entered.connect(_on_body_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body):
+	if body is CharacterBody3D:
+		print("In the inventory: ", body.inventory)
+		if body.inventory == Item && not completed:
+			body.inventory = ""
+			completed = true
+			print("COMPLETED")
