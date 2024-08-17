19fbf8ce91c1fbdf11349b5f4fe6fe4ede972e83
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-08-17 16:46:23 -0400

    Coyote and buffer

--- movimiento_arbol/Scripts/player_3d.gd
+++ movimiento_arbol/Scripts/player_3d.gd
@@ -3,6 +3,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var coyote_max = 0.12
+@export var jump_buffer_max = 0.2
+
+var coyote_timer = 0.0
+var jump_buffer_timer = 0.0
+var is_jumping = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var sprite_3d = $Sprite3D
@@ -22,8 +29,7 @@ func _physics_process(delta):
 		velocity.y += -gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	jump_logic(delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -50,3 +56,20 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func jump_logic(delta):
+	if is_on_floor():
+		coyote_timer = coyote_max
+		is_jumping = false
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = jump_buffer_max
+	
+	if coyote_timer > 0 and jump_buffer_timer > 0 and not is_jumping:
+		is_jumping = true
+		coyote_timer = 0
+		jump_buffer_timer = 0
+		
+		velocity.y = JUMP_VELOCITY
+	
+	coyote_timer -= delta
+	jump_buffer_timer -= delta
