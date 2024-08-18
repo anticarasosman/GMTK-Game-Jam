42a8f2879303679a5858796553cd6828b4c07f65
anticarasosman <Guillermo_Ossa@hotmail.com>
2024-08-17 22:10:27 -0400

    22:10

--- movimiento_arbol/Scripts/rotation_zone.gd
+++ movimiento_arbol/Scripts/rotation_zone.gd
@@ -1,9 +1,8 @@
 extends Area3D
-@onready var player_3d = $"../../Player3D"
+@onready var player_3d = $"../../../../Player3D"
 
 func _on_body_entered(_body):
 	player_3d.rotate = true
 
 func _on_body_exited(_body):
 	player_3d.rotate = false
-	pass
