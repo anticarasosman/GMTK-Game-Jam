7e73a98e2797e07a95362c645f003bf00d168614
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-08-18 18:18:39 -0400

    1818

--- movimiento_arbol/trash_scene.gd
+++ movimiento_arbol/trash_scene.gd
@@ -1,15 +1,28 @@
 extends Area3D
 
+@onready var player_3d = get_tree().get_current_scene().get_node("Player3D")
+
+var completed = false
+var Player_inside = false
+
+
 func _ready() -> void:
 	self.body_entered.connect(_on_body_entered)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	Pick_up()
 
 func _on_body_entered(body):
 	if body is CharacterBody3D:
-		if body.interacting:
-			body.inventory = ""
-			print("Cleared inventory")
+		Player_inside = true
+
+func _on_body_exited(body):
+	if body is CharacterBody3D:
+		Player_inside = false
+
+func Pick_up():
+	if Player_inside:
+		if player_3d.interacting and player_3d.inventory != "":
+			player_3d.inventory = ""
+			print("Trashed item!")
