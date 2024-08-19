925ec665a98386218262fda3b0b480b8b9f39cfa
anticarasosman <Guillermo_Ossa@hotmail.com>
2024-08-19 15:13:37 -0400

    15:13

--- movimiento_arbol/Scripts/arbol.gd
+++ movimiento_arbol/Scripts/arbol.gd
@@ -5,19 +5,16 @@ var Mass_Back = 0
 var Mass_Left = 0
 var Mass_Right = 0
 
-#Reemplazar los valores "5" del Vector3 por la masa de la decoracion
-func add_mass_front(mass):
-	var tween = create_tween()
-	tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(-mass,0,0), mass)
-		
-func add_mass_down(mass):
-	var tween = create_tween()
-	tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(mass,0,0), mass)
-
-func add_mass_left(mass):
-	var tween = create_tween()
-	tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(0,0,-mass), mass)
-		
-func add_mass_right(mass):
-	var tween = create_tween()
-	tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(0,0,mass), mass)
+func add_mass(mass, Zone):
+	if Zone == "Front":
+		var tween = create_tween()
+		tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(-mass,0,0), sqrt(mass))
+	elif Zone == "Back":
+		var tween = create_tween()
+		tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(mass,0,0), mass)
+	elif Zone == "Left":
+		var tween = create_tween()
+		tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(0,0,-mass), mass)
+	elif Zone == "Right":
+		var tween = create_tween()
+		tween.tween_property(self, "rotation_degrees", rotation_degrees-Vector3(0,0,mass), mass)
